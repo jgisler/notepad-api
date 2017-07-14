ac225a45aa1e032bd3990371e30887d579bf7236
Jim Gisler <jim.gisler@gmail.com>
2017-07-13 22:33:37 -0700

    Added request routing

--- index.js
+++ index.js
@@ -5,13 +5,34 @@ exports.handler = handleEvent;
 
 function handleEvent(event, context, callback) {
 
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: "Success"
-        })
-    };
+    console.log(`event=${JSON.stringify(event, null, 2)}`);
 
-    callback(null, response);
+    const resource = event.resource;
+    const httpMethod = event.httpMethod.toLowerCase();
 
+    switch (resource) {
+        case "/note/{noteId}":
+            switch (httpMethod) {
+                case 'get':
+                    callback(null, handleGet(event.pathParameters.noteId) );
+                    break;
+                default:
+                    callback(buildResponse(501, {message: 'Not implemented'}));
+            }
+            break;
+        default:
+            callback(buildResponse(501, {message: 'Not implemented'}));
+            break;
+    }
+}
+
+function buildResponse(statusCode, responseBody) {
+    return {
+        statusCode: statusCode,
+        body: JSON.stringify(responseBody)
+    }
+}
+
+function handleGet(noteId) {
+    return buildResponse(200, {note: {noteId: noteId}});
 }
